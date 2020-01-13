f4682e2f6840fc8dfbddbfdeed369d011e2c7e52
Pedro Silva <1540968@TCS.com>
2020-01-13 16:21:48 -0300

    Edit, Delete, Datepicker

--- crud-cliente/view-model/ClienteViewModel.cs
+++ crud-cliente/view-model/ClienteViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using common.CustomAttributes;
 using common.Enums;
 using System;
+using System.ComponentModel;
 
 namespace view_model
 {
@@ -12,16 +13,31 @@ namespace view_model
         public string Nome { get; set; }
 
         [RequiredValidation(FieldType: typeof(DateTime), ErrorMessage = "O campo Data de Nascimento é obrigatório")]
+        [DisplayName("Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
 
         [RequiredValidation(FieldType: typeof(int), ErrorMessage = "O campo Sexo é obrigatório")]
         public ESexo Sexo { get; set; }
         public string Cep { get; set; }
+
+        [DisplayName("Endereço")]
         public string Endereco { get; set; }
+
+        [DisplayName("Número")]
         public string Numero { get; set; }
         public string Complemento { get; set; }
         public string Bairro { get; set; }
+
+        [DisplayName("UF")]
         public EEstado? Estado { get; set; }
         public string Cidade { get; set; }
+
+        public string DataNascimentoFormatada
+        {
+            get
+            {
+                return DataNascimento.ToString("dd/MM/yyyy");
+            }
+        }
     }    
 }
